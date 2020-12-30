30da32e1d8d06024f3695fe6d1452a8fc89b7348
unknown <abdelhakim@univ-tiaret.dz>
2020-12-30 18:32:25 +0100

    train start

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const net = new brain.NeuralNetwork()
 net.train([
 
     {
-        input:
-        output:
-
-
-
-    }
+        input:[1,0],
+        output:[0]
+    },
+    {
+        input:[0,1],
+        output:[1]
+    },
 ])
 
 const diagram= document.getElementById('diagram')
